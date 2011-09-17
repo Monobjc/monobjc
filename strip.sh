fa2d0d8793382848607aa1bd7ffb1886b82b1cb4
Laurent Etiemble <laurent.etiemble@monobjc.net>
2011-09-17 21:43:26 +0200

    Update tools. Fix compilation warnings. Update license.

--- strip.sh
+++ strip.sh
@@ -5,7 +5,7 @@ FOLDER=stripped
 rm -Rf $FOLDER.libraries
 
 # Get all the files from the src folder
-find libraries -type f | sed '/.svn/d' > .allfiles
+find libraries -type f > .allfiles
 
 # Filter source files
 egrep ".cs$" .allfiles > .sourcefiles
@@ -16,17 +16,17 @@ egrep -v ".cs$" .allfiles > .otherfiles
 for file in `cat .otherfiles`
 do
 	echo "Processing $file..."
-	mkdir -p "$FOLDER.$file"
-	rmdir "$FOLDER.$file"
-	cp "$file" "$FOLDER.$file"
+    mkdir -p "$FOLDER/$file"
+    rm -Rf "$FOLDER/$file"
+	cp "$file" "$FOLDER/$file"
 done
 
 for file in `cat .sourcefiles`
 do
 	echo "Stripping $file..."
-	mkdir -p "$FOLDER.$file"
-	rmdir "$FOLDER.$file"
-	sed '/\/\/\//d' "$file" > "$FOLDER.$file"
+    mkdir -p "$FOLDER/$file"
+    rm -Rf "$FOLDER/$file"
+	sed '/\/\/\//d' "$file" > "$FOLDER/$file"
 done
 
 rm -f .allfiles
