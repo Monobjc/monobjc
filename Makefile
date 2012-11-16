7675ce0ec08f2de2f6f6c8200024c6bf2ee41c1d
Laurent Etiemble <laurent.etiemble@monobjc.net>
2012-11-16 08:09:08 +0100

    Complete the makefiles.

--- Makefile
+++ Makefile
@@ -14,15 +14,27 @@ include Monobjc.mk
 # Variables
 # ----------------------------------------
 
-DIRS = native libraries
+DIRS= \
+	native \
+	tools \
+	libraries
+
+# ----------------------------------------
+# Functions
+# ----------------------------------------
+
+make_dir_conditional= \
+	if [ $(1) = 1 ]; then \
+		$(MKDIR) $(2); \
+	fi
 
 # ----------------------------------------
 # Targets
 # ----------------------------------------
 
 all:
-	mkdir -p "$(BUILD_DIR)"
-	mkdir -p "$(DIST_DIR)"
+	$(MKDIR) "$(BUILD_DIR)"
+	$(MKDIR) "$(DIST_DIR)"
 	$(call make_dir_conditional,$(BUILD_FOR_OSX_10_6),$(DIST_DIR)/10.6)
 	$(call make_dir_conditional,$(BUILD_FOR_OSX_10_7),$(DIST_DIR)/10.7)
 	$(call make_dir_conditional,$(BUILD_FOR_OSX_10_8),$(DIST_DIR)/10.8)
@@ -34,5 +46,5 @@ clean:
 	for i in $(DIRS); do \
 		(cd $$i; make clean); \
 	done;
-	rm -Rf "$(BUILD_DIR)"
-	rm -Rf "$(DIST_DIR)"
+	$(RMF) "$(BUILD_DIR)"
+	$(RMF) "$(DIST_DIR)"
--- Monobjc.mk
+++ Monobjc.mk
@@ -6,59 +6,49 @@
 # ----------------------------------------
 
 # Set the directories
-export DOC_DIR ?= $(CURDIR)/doc
-export EXTERNAL_DIR ?= $(CURDIR)/external
-export LIBRARIES_DIR ?= $(CURDIR)/libraries
-export NATIVE_DIR ?= $(CURDIR)/native
-export BUILD_DIR ?= $(CURDIR)/build
-export DIST_DIR ?= $(CURDIR)/dist
+export DOC_DIR?=$(CURDIR)/doc
+export EXTERNAL_DIR?=$(CURDIR)/external
+export LIBRARIES_DIR?=$(CURDIR)/libraries
+export NATIVE_DIR?=$(CURDIR)/native
+export BUILD_DIR?=$(CURDIR)/build
+export DIST_DIR?=$(CURDIR)/dist
 
 # Set the files
-export KEY_FILE ?= $(CURDIR)/Monobjc.snk
+export KEY_FILE?=$(CURDIR)/Monobjc.snk
 
 # Set the build parameters
-DEBUG_VALUE=false
-TESTING_VALUE=false
-NATIVE_ARCHS=i386
+export DEBUG_VALUE=false
+export TESTING_VALUE=false
+export NATIVE_ARCHS=i386
 
 # Set the tools
-MCS = dmcs
-RESGEN = resgen
+export CPC=rsync -a
+export MCS=dmcs
 ifeq ($(TESTING_VALUE),false )
 	MCS+= -keyfile:$(KEY_FILE)
 endif
+export MKDIR=mkdir -p
+export RESGEN=resgen
+export RMF=rm -Rf
+export XBUILD=xbuild /p:Configuration=Release /verbosity:quiet
 
 # Compute the version parts
-MONOBJC_VERSION=5.0
-MONOBJC_VERSION_MAJOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f1)
-MONOBJC_VERSION_MINOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f2)
+export MONOBJC_VERSION=5.0
+export MONOBJC_VERSION_MAJOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f1)
+export MONOBJC_VERSION_MINOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f2)
 
-MIN_OSX_VERSION=10.6
-MIN_OSX_VERSION_MAJOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f1)
-MIN_OSX_VERSION_MINOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f2)
-MAX_OSX_VERSION=10.8
-MAX_OSX_VERSION_MAJOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f1)
-MAX_OSX_VERSION_MINOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f2)
+export MIN_OSX_VERSION=10.6
+export MIN_OSX_VERSION_MAJOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f1)
+export MIN_OSX_VERSION_MINOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f2)
+export MAX_OSX_VERSION=10.8
+export MAX_OSX_VERSION_MAJOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f1)
+export MAX_OSX_VERSION_MINOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f2)
 
-BUILD_FOR_OSX_10_6=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=6 && $(MAX_OSX_VERSION_MINOR) >=6" | bc)
-BUILD_FOR_OSX_10_7=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=7 && $(MAX_OSX_VERSION_MINOR) >=7" | bc)
-BUILD_FOR_OSX_10_8=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=8 && $(MAX_OSX_VERSION_MINOR) >=8" | bc)
+export BUILD_FOR_OSX_10_6=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=6 && $(MAX_OSX_VERSION_MINOR) >=6" | bc)
+export BUILD_FOR_OSX_10_7=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=7 && $(MAX_OSX_VERSION_MINOR) >=7" | bc)
+export BUILD_FOR_OSX_10_8=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=8 && $(MAX_OSX_VERSION_MINOR) >=8" | bc)
 
 # Compute the build number
-DATE_REFERENCE=$(shell date -j -f "%Y-%m-%d" "2007-07-01" "+%s")
-DATE_TODAY=$(shell date "+%s")
-REVISION_NUMBER=$(shell echo "($(DATE_TODAY) - $(DATE_REFERENCE)) / 86400" | bc)
-
-# ----------------------------------------
-# Functions
-# ----------------------------------------
-
-make_dir_conditional= \
-	if [ $(1) = 1 ]; then \
-		mkdir -p $(2); \
-	fi
-
-copy_files_conditional= \
-	if [ $(1) = 1 ]; then \
-		rsync -a --exclude="tmp" "$(2)" "$(3)"; \
-	fi
+export DATE_REFERENCE=$(shell date -j -f "%Y-%m-%d" "2007-07-01" "+%s")
+export DATE_TODAY=$(shell date "+%s")
+export REVISION_NUMBER=$(shell echo "($(DATE_TODAY) - $(DATE_REFERENCE)) / 86400" | bc)
