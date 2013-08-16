3da968920885632518a799b9f8eba8d1646bbd75
Aaron Oneal <aaron.oneal@spicypixel.com>
2013-08-15 17:04:16 -0700

    Enable command-line override for OSX versions to build.

--- Monobjc.mk
+++ Monobjc.mk
@@ -57,16 +57,16 @@ export MONOBJC_VERSION=5.1
 export MONOBJC_VERSION_MAJOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f1)
 export MONOBJC_VERSION_MINOR=$(shell echo $(MONOBJC_VERSION) | cut -d "." -f2)
 
-export MIN_OSX_VERSION=10.6
+export MIN_OSX_VERSION?=10.6
 export MIN_OSX_VERSION_MAJOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f1)
 export MIN_OSX_VERSION_MINOR=$(shell echo $(MIN_OSX_VERSION) | cut -d "." -f2)
-export MAX_OSX_VERSION=10.8
+export MAX_OSX_VERSION?=10.8
 export MAX_OSX_VERSION_MAJOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f1)
 export MAX_OSX_VERSION_MINOR=$(shell echo $(MAX_OSX_VERSION) | cut -d "." -f2)
 
-export BUILD_FOR_OSX_10_6=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=6 && $(MAX_OSX_VERSION_MINOR) >=6" | bc)
-export BUILD_FOR_OSX_10_7=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=7 && $(MAX_OSX_VERSION_MINOR) >=7" | bc)
-export BUILD_FOR_OSX_10_8=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=8 && $(MAX_OSX_VERSION_MINOR) >=8" | bc)
+export BUILD_FOR_OSX_10_6?=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=6 && $(MAX_OSX_VERSION_MINOR) >=6" | bc)
+export BUILD_FOR_OSX_10_7?=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=7 && $(MAX_OSX_VERSION_MINOR) >=7" | bc)
+export BUILD_FOR_OSX_10_8?=$(shell echo "$(MIN_OSX_VERSION_MINOR) <=8 && $(MAX_OSX_VERSION_MINOR) >=8" | bc)
 
 # Compute the build number
 export DATE_REFERENCE=$(shell date -j -f "%Y-%m-%d" "2007-07-01" "+%s")
