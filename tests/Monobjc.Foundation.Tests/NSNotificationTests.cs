d9a8835df5afaf6996142f993ffff3599d246f03
Laurent Etiemble <laurent.etiemble@monobjc.net>
2013-08-26 22:54:42 +0200

    Fix typo and make const.

--- tests/Monobjc.Foundation.Tests/NSNotificationTests.cs
+++ tests/Monobjc.Foundation.Tests/NSNotificationTests.cs
@@ -85,7 +85,7 @@ namespace Monobjc.Foundation
     {
         public static Class MyListenerClass = Class.Get(typeof (MyListener));
 		
-		public static readonly String SELECTOR_NOTIFICATION = new NSString("SELECTOR_NOTIFICATION");
+        public const String SELECTOR_NOTIFICATION = "SELECTOR_NOTIFICATION";
 		
         public MyListener()
         {
