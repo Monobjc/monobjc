ca2a315a83ac369ecad5d9dbe3606c9559c12305
Laurent Etiemble <laurent.etiemble@monobjc.net>
2013-11-06 22:45:50 +0100

    CoreText framework is not available on OX 10.7. So remove it from the dependent frameworks.

--- libraries/Monobjc.Foundation/Properties/MonobjcAssemblyInfo.cs
+++ libraries/Monobjc.Foundation/Properties/MonobjcAssemblyInfo.cs
@@ -23,7 +23,7 @@
 using Monobjc;
 using System.Reflection;
 
-[assembly: ObjectiveCFramework(true, "Foundation", "CoreData", "CoreText")]
+[assembly: ObjectiveCFramework(true, "Foundation", "CoreData")]
 [assembly: AssemblyConfigurationAttribute("Release")]
 
 #if MACOSX_10_8
