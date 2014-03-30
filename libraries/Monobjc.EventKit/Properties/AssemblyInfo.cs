8ae3896fbe3d2481769bf2c9685951c384c457cc
Laurent Etiemble <laurent.etiemble@monobjc.net>
2014-03-30 17:23:11 +0200

    Update copyright on .NET sources.

--- libraries/Monobjc.EventKit/Properties/AssemblyInfo.cs
+++ libraries/Monobjc.EventKit/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 //
 // This file is part of Monobjc, a .NET/Objective-C bridge
-// Copyright (C) 2007-2013 - Laurent Etiemble
+// Copyright (C) 2007-2014 - Laurent Etiemble
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Monobjc Bridge EventKit Library")]
 [assembly: AssemblyCompany("Monobjc Project")]
 [assembly: AssemblyProduct("Monobjc Bridge Project")]
-[assembly: AssemblyCopyright("Copyright (c) Monobjc Project 2007-2012 - Licensed under MIT License")]
+[assembly: AssemblyCopyright("Copyright (c) Monobjc Project 2007-2014 - Licensed under MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
