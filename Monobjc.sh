523d5d3e181374e756ad0fa717079c4ff67d59d7
Laurent Etiemble <laurent.etiemble@monobjc.net>
2014-09-11 08:11:26 +0200

    Fix Foundation classes.Finish the OS X v10.9 mappings. Fix install script.

--- Monobjc.sh
+++ Monobjc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "======================================="
-echo " __  __                   _     _      "
+echo " __  __				   _	 _	  "
 echo "|  \/  | ___  _ __   ___ | |__ (_) ___ "
 echo "| |\/| |/ _ \| '_ \ / _ \| '_ \| |/ __|"
 echo "| |  | | (_) | | | | (_) | |_) | | (__ "
 echo "|_|  |_|\___/|_| |_|\___/|_.__// |\___|"
-echo "                             |__/      "
-echo "Installer                              "
+echo "							 |__/	  "
+echo "Installer							  "
 echo "======================================="
 echo
 
@@ -34,57 +34,57 @@ fi
 #
 function install {
 
-    VERSIONS="10.6 10.7 10.8 10.9"
+	VERSIONS="10.7 10.8 10.9"
 
-    # Perform the installation for each version
-    for version in $VERSIONS; do
-        echo
-        echo "Installing Monobjc $version..."
-        echo "=============================="
+	# Perform the installation for each version
+	for version in $VERSIONS; do
+		echo
+		echo "Installing Monobjc $version..."
+		echo "=============================="
 	
-        LIB_DIR="$MONO_DIR/lib/mono/monobjc-$version"
+		LIB_DIR="$MONO_DIR/lib/mono/monobjc-$version"
 
-        # Create directory
-        mkdir -p "$LIB_DIR"
-    
-        # Copy native elements
-        cp "./dist/$version/monobjc.h" "$LIB_DIR"
-        
-	    if [ $UNIVERSAL == "1" ]; then
-	        cp "./dist/$version/libmonobjc.dylib" "$LIB_DIR"
-    	    cp "./dist/$version/runtime" "$LIB_DIR"
-	    else
-            cp "./dist/$version/libmonobjc.dylib" "$LIB_DIR"
-            cp "./dist/$version/runtime" "$LIB_DIR"
-    		lipo -extract i386 "./dist/$version/libmonobjc.dylib" -output "$LIB_DIR/libmonobjc.dylib"
-    		lipo -extract i386 "./dist/$version/runtime" -output "$LIB_DIR/runtime"
-	    fi
-        
-        chmod a+rx "$LIB_DIR/libmonobjc.dylib"
-        chmod a+rx "$LIB_DIR/runtime"
-
-        # Copy the documentation
-        for file in `ls dist/$version/Monobjc*.xml`; do
-            cp $file "$LIB_DIR"
-        done
-    
-        # Register the Monobjc assemblies in the GAC
-        for file in `ls dist/$version/Monobjc*.dll`; do
-            gacutil -i "$file" -package monobjc-$version
-        done
-
-        # Create the list of assemblies
-        LIB_LIST=""
-        LIB_REFERENCES="Libs:"
-        for file in `ls dist/$version/Monobjc*.dll`; do
-            assembly=`basename $file`
-            LIB_LIST="$LIB_LIST\${libdir}/$assembly "
-            LIB_REFERENCES="$LIB_REFERENCES -r:\${libdir}/$assembly"
-        done
+		# Create directory
+		mkdir -p "$LIB_DIR"
 	
-        # Create the PKG-CONFIG file
-        PC_FILE="$MONO_DIR/lib/pkgconfig/monobjc-$version.pc"
-        cat > "$PC_FILE" <<EOF
+		# Copy native elements
+		cp "./dist/$version/monobjc.h" "$LIB_DIR"
+		
+		if [ $UNIVERSAL == "1" ]; then
+			cp "./dist/$version/libmonobjc.dylib" "$LIB_DIR"
+			cp "./dist/$version/runtime" "$LIB_DIR"
+		else
+			cp "./dist/$version/libmonobjc.dylib" "$LIB_DIR"
+			cp "./dist/$version/runtime" "$LIB_DIR"
+			lipo -extract i386 "./dist/$version/libmonobjc.dylib" -output "$LIB_DIR/libmonobjc.dylib"
+			lipo -extract i386 "./dist/$version/runtime" -output "$LIB_DIR/runtime"
+		fi
+		
+		chmod a+rx "$LIB_DIR/libmonobjc.dylib"
+		chmod a+rx "$LIB_DIR/runtime"
+
+		# Copy the documentation
+		for file in `ls dist/$version/Monobjc*.xml`; do
+			cp $file "$LIB_DIR"
+		done
+	
+		# Register the Monobjc assemblies in the GAC
+		for file in `ls dist/$version/Monobjc*.dll`; do
+			gacutil -i "$file" -package monobjc-$version
+		done
+
+		# Create the list of assemblies
+		LIB_LIST=""
+		LIB_REFERENCES="Libs:"
+		for file in `ls dist/$version/Monobjc*.dll`; do
+			assembly=`basename $file`
+			LIB_LIST="$LIB_LIST\${libdir}/$assembly "
+			LIB_REFERENCES="$LIB_REFERENCES -r:\${libdir}/$assembly"
+		done
+	
+		# Create the PKG-CONFIG file
+		PC_FILE="$MONO_DIR/lib/pkgconfig/monobjc-$version.pc"
+		cat > "$PC_FILE" <<EOF
 prefix=$MONO_DIR
 exec_prefix=\${prefix}
 libdir=\${exec_prefix}/lib/mono/monobjc-$version
@@ -96,23 +96,21 @@ Version: $version
 
 $LIB_REFERENCES
 EOF
-    done
+	done
 
-    # Copy the helper tools
-    cp "./dist/Monobjc.Tools.Sdp.exe" "$MONO_DIR/lib/mono/4.0/Monobjc.Tools.Sdp.exe"
-    
-    # Copy the runtime binary
-    cp "./dist/monobjc" "$MONO_DIR/bin/monobjc"
-
-    # Copy the runtime wrappers and soft-link them
-    cp "./dist/monobjc-sdp" "$MONO_DIR/bin/monobjc-sdp"
-    cp "./dist/monobjc-nunit" "$MONO_DIR/bin/monobjc-nunit"
-    chmod a+rx "$MONO_DIR/bin/monobjc"
-    chmod a+rx "$MONO_DIR/bin/monobjc-sdp"
-    chmod a+rx "$MONO_DIR/bin/monobjc-nunit"
-    ln -s "$MONO_DIR/bin/monobjc" "/usr/bin/monobjc"
-    ln -s "$MONO_DIR/bin/monobjc-sdp" "/usr/bin/monobjc-sdp"
-    ln -s "$MONO_DIR/bin/monobjc-nunit" "/usr/bin/monobjc-nunit"
+	# Copy the helper tools
+	cp "./dist/Monobjc.Tools.Sdp.exe" "$MONO_DIR/lib/mono/4.0/Monobjc.Tools.Sdp.exe"
+	
+	# Copy the runtime binary and wrappers and soft-link them
+	cp "./dist/monobjc" "$MONO_DIR/bin/monobjc"
+	cp "./dist/monobjc-sdp" "$MONO_DIR/bin/monobjc-sdp"
+	cp "./dist/monobjc-nunit" "$MONO_DIR/bin/monobjc-nunit"
+	chmod a+rx "$MONO_DIR/bin/monobjc"
+	chmod a+rx "$MONO_DIR/bin/monobjc-sdp"
+	chmod a+rx "$MONO_DIR/bin/monobjc-nunit"
+	ln -s "$MONO_DIR/bin/monobjc" "/usr/bin/monobjc"
+	ln -s "$MONO_DIR/bin/monobjc-sdp" "/usr/bin/monobjc-sdp"
+	ln -s "$MONO_DIR/bin/monobjc-nunit" "/usr/bin/monobjc-nunit"
 }
 
 #
@@ -120,65 +118,65 @@ EOF
 #
 function uninstall {
 
-    VERSIONS="10.6 10.7 10.8 10.9"
-    ASSEMBLIES=`gacutil -l | grep Monobjc | awk -F"," '{ print $1 }' | sort -u`
+	VERSIONS="10.7 10.8 10.9"
+	ASSEMBLIES=`gacutil -l | grep Monobjc | awk -F"," '{ print $1 }' | sort -u`
 
-    # Remove assemblies from the GAC
-    for assembly in $ASSEMBLIES; do
-        gacutil -u $assembly
-    done
-    
-    # Perform the installation for each version
-    for version in $VERSIONS; do
-        echo
-        echo "Uninstalling Monobjc $version..."
-        echo "================================"
+	# Remove assemblies from the GAC
+	for assembly in $ASSEMBLIES; do
+		gacutil -u $assembly
+	done
 	
-        LIB_DIR="$MONO_DIR/lib/mono/monobjc-$version"
-        
-        rm -Rf $LIB_DIR
-    done
+	# Perform the installation for each version
+	for version in $VERSIONS; do
+		echo
+		echo "Uninstalling Monobjc $version..."
+		echo "================================"
+	
+		LIB_DIR="$MONO_DIR/lib/mono/monobjc-$version"
+		
+		rm -Rf $LIB_DIR
+	done
 
-    # Remove executables
-    rm -f "/usr/bin/monobjc"
-    rm -f "/usr/bin/monobjc-nunit"
-    rm -f "$MONO_DIR/bin/monobjc"
-    rm -f "$MONO_DIR/bin/monobjc-nunit"
-    
-    # Remove the helper tools
-    rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.Tools.Sdp.exe"
+	# Remove executables
+	rm -f "/usr/bin/monobjc"
+	rm -f "/usr/bin/monobjc-nunit"
+	rm -f "$MONO_DIR/bin/monobjc"
+	rm -f "$MONO_DIR/bin/monobjc-nunit"
+	
+	# Remove the helper tools
+	rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.Tools.Sdp.exe"
 }
 
 function install_nant {
-    EXT_DIR="$MONO_DIR/share/NAnt/bin/extensions/common/4.0/"
+	EXT_DIR="$MONO_DIR/share/NAnt/bin/extensions/common/4.0/"
 
-    # Copy the NAnt tasks
-    mkdir -p "$EXT_DIR"
-    cp "./dist/Monobjc.NAnt.dll" "$EXT_DIR"
+	# Copy the NAnt tasks
+	mkdir -p "$EXT_DIR"
+	cp "./dist/Monobjc.NAnt.dll" "$EXT_DIR"
 }
 
 function uninstall_nant {
-    EXT_DIR="$MONO_DIR/share/NAnt/bin/extensions/common/4.0/"
+	EXT_DIR="$MONO_DIR/share/NAnt/bin/extensions/common/4.0/"
 
-    # Remove the NAnt tasks
-    rm -f "$EXT_DIR/Monobjc.NAnt.dll"
+	# Remove the NAnt tasks
+	rm -f "$EXT_DIR/Monobjc.NAnt.dll"
 }
 
 function install_msbuild {
-    gacutil -i "./dist/Monobjc.MSBuild.dll"
+	gacutil -i "./dist/Monobjc.MSBuild.dll"
 
-    # Copy the MSBuild tasks and targets
-    ln -s "$MONO_DIR/lib/mono/gac/Monobjc.MSBuild/$VERSION""__""$HASH/Monobjc.MSBuild.dll" "$MONO_DIR/lib/mono/4.0"
-    cp ./dist/Monobjc.*.tasks "$MONO_DIR/lib/mono/4.0/"
-    cp ./dist/Monobjc.*.targets "$MONO_DIR/lib/mono/4.0/"
+	# Copy the MSBuild tasks and targets
+	ln -s "$MONO_DIR/lib/mono/gac/Monobjc.MSBuild/$VERSION""__""$HASH/Monobjc.MSBuild.dll" "$MONO_DIR/lib/mono/4.0"
+	cp ./dist/Monobjc.*.tasks "$MONO_DIR/lib/mono/4.0/"
+	cp ./dist/Monobjc.*.targets "$MONO_DIR/lib/mono/4.0/"
 }
 
 function uninstall_msbuild {
-    # Remove the MSBuild tasks and targets
-    rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.Build.dll"
-    rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.MSBuild.dll"
-    rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.*.tasks"
-    rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.*.targets"
+	# Remove the MSBuild tasks and targets
+	rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.Build.dll"
+	rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.MSBuild.dll"
+	rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.*.tasks"
+	rm -f "$MONO_DIR/lib/mono/4.0/Monobjc.*.targets"
 }
 
 function install_monodoc {
@@ -191,40 +189,40 @@ function install_monodoc {
 # Main entry point
 case "$COMMAND" in
 
-    install)
-        uninstall
-        install
-        ;;
-    
-    uninstall)
-        uninstall
-        ;;
+	install)
+		uninstall
+		install
+		;;
+	
+	uninstall)
+		uninstall
+		;;
 
-    install_nant)
-        uninstall_nant
-        install_nant
-        ;;
+	install_nant)
+		uninstall_nant
+		install_nant
+		;;
 
-    uninstall_nant)
-        uninstall_nant
-        ;;
+	uninstall_nant)
+		uninstall_nant
+		;;
 
-    install_msbuild)
-        uninstall_msbuild
-        install_msbuild
-        ;;
+	install_msbuild)
+		uninstall_msbuild
+		install_msbuild
+		;;
 
-    uninstall_msbuild)
-        uninstall_msbuild
-        ;;
+	uninstall_msbuild)
+		uninstall_msbuild
+		;;
 
-    install_monodoc)
-        install_monodoc
-        ;;
+	install_monodoc)
+		install_monodoc
+		;;
 
-    *)
-        echo $"Usage: $0 {install|uninstall|install_nant|uninstall_nant|install_msbuild|uninstall_msbuild|install_monodoc}"
-        exit 1
-        ;;
+	*)
+		echo $"Usage: $0 {install|uninstall|install_nant|uninstall_nant|install_msbuild|uninstall_msbuild|install_monodoc}"
+		exit 1
+		;;
 
 esac
---
+++ libraries/Monobjc.Foundation/Foundation_Extensions/SecurityTransport.Forward.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Monobjc.Foundation
+{
+    /// <summary>
+    /// Represents the SSL protocol version.
+    /// </summary>
+    /// <remarks>>Forward declaration to avoid circular dependencies between Foundation and Security.</remarks>
+    public enum SSLProtocol
+    {
+        /// <summary>
+        /// Specifies that no protocol has been or should be negotiated or specified; use default.
+        /// </summary>
+        kSSLProtocolUnknown = 0,
+        /// <summary>
+        /// Specifies that the SSL 3.0 protocol is preferred; the SSL 2.0 protocol may be negotiated if the peer cannot use the SSL 3.0 protocol.
+        /// </summary>
+        kSSLProtocol3 = 2,
+        /// <summary>
+        /// Specifies that the TLS 1.0 protocol is preferred but lower versions may be negotiated.
+        /// </summary>
+        kTLSProtocol1 = 4,
+        /// <summary>
+        /// Specifies that the TLS 1.1 protocol is preferred but lower versions may be negotiated.
+        /// </summary>
+        kTLSProtocol11 = 7,
+        /// <summary>
+        /// Specifies that the TLS 1.2 protocol is preferred but lower versions may be negotiated.
+        /// </summary>
+        kTLSProtocol12 = 8,
+        /// <summary>
+        /// Specifies that the DTLS 1.0 protocol is preferred but lower versions may be negotiated.
+        /// </summary>
+        kDTLSProtocol1 = 9,
+        /// <summary>
+        /// Specifies that only the SSL 2.0 protocol may be negotiated. Deprecated in iOS.
+        /// </summary>
+        kSSLProtocol2 = 1,
+        /// <summary>
+        /// Specifies that only the SSL 3.0 protocol may be negotiated; fails if the peer tries to negotiate the SSL 2.0 protocol. Deprecated in iOS.
+        /// </summary>
+        kSSLProtocol3Only = 3,
+        /// <summary>
+        /// Specifies that only the TLS 1.0 protocol may be negotiated. Deprecated in iOS.
+        /// </summary>
+        kTLSProtocol1Only = 5,
+        /// <summary>
+        /// Specifies all supported versions. Deprecated in iOS.
+        /// </summary>
+        kSSLProtocolAll = 6,
+    }
+}
